409b114656b47be451fffef561cae96c6357d148
willen <willen.sesantos@gmail.com>
2023-06-17 15:46:46 -0300

    Exception handling - Delete

--- src/main/java/com/edweb/course/services/UserService.java
+++ src/main/java/com/edweb/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.edweb.course.entities.User;
 import com.edweb.course.repositories.UserRepository;
+import com.edweb.course.services.exceptions.DatabaseException;
 import com.edweb.course.services.exceptions.ResourceNotFoundException;
 
 // Camadas de regras de negócio
@@ -32,7 +35,13 @@ public class UserService {
     }
 
     public void delete(Long id) {
-        repository.deleteById(id);
+        try {
+            repository.deleteById(id);
+        } catch (EmptyResultDataAccessException e) {
+            throw new ResourceNotFoundException(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException(e.getMessage());
+        }
     }
 
     public User update(Long id, User obj) {
